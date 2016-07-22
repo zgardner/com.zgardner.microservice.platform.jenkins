f59c72d39c77cdca01da9eb3a7ad123e20e465a1
Zach Gardner <z.gardner@hotmail.com>
2016-07-21 21:20:50 -0500

    Added the docker file to extend the base Jenkins image, a build and a run file.

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t com.zgardner.microservice.platform.jenkins:latest .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 8080:8080 -p 50000:50000 -v /home/zgardner/jenkins/data:/var/jenkins_home com.zgardner.microservice.platform.jenkins:latest
